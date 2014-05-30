0042ec633407b64c0f2a454daef2ce2f061d165e
mmontone <marianomontone@gmail.com>
2014-05-29 23:04:42 -0300

    Tests structure and successful tests

--- test.lisp
+++ test.lisp
@@ -1,116 +1,128 @@
 (defpackage docstring-parser.test
-  (:use :cl :docstring-parser :esrap :stefil))
+  (:use :cl :docstring-parser :esrap :stefil)
+  (:export :run-tests))
 
 (in-package :docstring-parser.test)
 
-(let ((newline "
+(defun run-tests ()
+  (without-debugging
+    (docstring-parser-test)))
+
+(in-root-suite)
+
+(defsuite docstring-parser-test)
+
+(in-suite docstring-parser-test)
+
+(deftest newline-test ()
+  (let ((newline "
 "))
-  (parse 'docstring-parser::eol newline))
+    (parse 'docstring-parser::eol newline))
 
-(signals error
-  (let ((newline "lala"))
-    (parse 'docstring-parser::eol newline)))
+  (signals error
+    (let ((newline "lala"))
+      (parse 'docstring-parser::eol newline))))
 
-;; spacing
+(deftest spacing-test ()
 
-(let ((spaces "     "))
-  (parse 'docstring-parser::spacing spaces))
+  (let ((spaces "     "))
+    (parse 'docstring-parser::spacing spaces))
 
-(let ((spaces (coerce '(#\Tab #\Tab #\  ) 'string)))
-  (parse 'docstring-parser::spacing spaces))
+  (let ((spaces (coerce '(#\Tab #\Tab #\  ) 'string)))
+    (parse 'docstring-parser::spacing spaces))
 
-(signals error
+  (signals error
+    (let ((spaces "
+                  "))
+      (parse 'docstring-parser::spacing spaces))))
+
+(deftest spacing*-test ()
+
+  (let ((spaces "     "))
+    (parse 'docstring-parser::spacing* spaces))
+
+  (let ((spaces (coerce '(#\Tab #\Tab #\  ) 'string)))
+    (parse 'docstring-parser::spacing* spaces))
+
   (let ((spaces "
                   "))
-  (parse 'docstring-parser::spacing spaces)))
+    (parse 'docstring-parser::spacing* spaces))
 
-;; spacing*
-
-(let ((spaces "     "))
-  (parse 'docstring-parser::spacing* spaces))
-
-(let ((spaces (coerce '(#\Tab #\Tab #\  ) 'string)))
-  (parse 'docstring-parser::spacing* spaces))
-
-(let ((spaces "
-                  "))
-  (parse 'docstring-parser::spacing* spaces))
-
-(signals error
-  (let ((text "    asfasdf  "))
-    (parse 'docstring-parser::spacing* text)))
-
-(let ((word "hello"))
-  (parse 'docstring-parser::word word))
-
-(signals error
- (let ((word "hello "))
-  (parse 'docstring-parser::word word)))
-
-(let ((word "**-asdf_%asdf"))
-  (parse 'docstring-parser::word word))
-
-;; Text
-
-(let ((text "asdf asdf; hdsf"))
-  (parse 'docstring-parser::text text))
-
-(let ((text "     asdfasdf  "))
-  (parse 'docstring-parser::text text))
-
-(let ((text " asdfasdf
-              asdfas"))
   (signals error
-    (parse 'docstring-parser::text-line text)))
+    (let ((text "    asfasdf  "))
+      (parse 'docstring-parser::spacing* text))))
 
-(let ((text " asdfasdf
+(deftest word-test ()
+  (let ((word "hello"))
+    (parse 'docstring-parser::word word))
+
+  (signals error
+    (let ((word "hello "))
+      (parse 'docstring-parser::word word)))
+
+  (let ((word "**-asdf_%asdf"))
+    (parse 'docstring-parser::word word)))
+
+(deftest text-test ()
+
+  (let ((text "asdf asdf; hdsf"))
+    (parse 'docstring-parser::text text))
+
+  (let ((text "     asdfasdf  "))
+    (parse 'docstring-parser::text text))
+
+  (let ((text " asdfasdf
               asdfas"))
-  (parse 'docstring-parser::text text))
+    (signals error
+      (parse 'docstring-parser::text-line text)))
 
-;; Code
+  (let ((text " asdfasdf
+              asdfas"))
+    (parse 'docstring-parser::text text)))
 
-(let ((code-element "``(+ 2 `(print ,'asdf))``"))
-  (parse 'docstring-parser::code-element code-element))
+(deftest code-test  ()
 
-(let ((code-element "``(let ((x 22))
+  (let ((code-element "``(+ 2 `(print ,'asdf))``"))
+    (parse 'docstring-parser::code-element code-element))
+
+  (let ((code-element "``(let ((x 22))
                              (print x))``"))
-  (parse 'docstring-parser::code-element code-element))
+    (parse 'docstring-parser::code-element code-element)))
 
-;; Lists
+(deftest list-test ()
 
-(let ((list-item "* my item"))
-  (parse 'docstring-parser::list-item list-item))
+  (let ((list-item "* my item"))
+    (parse 'docstring-parser::list-item list-item))
 
-(let ((list-item "* my item
+  (let ((list-item "* my item
                     has lots of lines!!"))
-   (parse 'docstring-parser::list-item list-item))
+    (parse 'docstring-parser::list-item list-item))
 
-(let ((list-item "      * an item"))
-  (parse 'docstring-parser::list-item list-item))
+  (let ((list-item "      * an item"))
+    (parse 'docstring-parser::list-item list-item))
 
-(let ((list-item "   * * an item"))
-  (parse 'docstring-parser::list-item list-item))
+  (let ((list-item "   * * an item"))
+    (parse 'docstring-parser::list-item list-item))
 
-(let ((list "* first item
+  (let ((list "* first item
              * second item"))
-  (parse 'docstring-parser::list-element list))
+    (parse 'docstring-parser::list-element list))
 
-
-(let ((list "* first item
+  (let ((list "* first item
                asdfasdfasdf
                asdfasdfsf
              * second item
              * third * third"))
-  (parse 'docstring-parser::list-element list))
+    (parse 'docstring-parser::list-element list)))
 
-;; Output normalization
+(deftest output-normalization-test ()
 
-(is
- (equalp (docstring-parser::concat-inbetween-text '("foo" "bar"))
-	 (list "foobar")))
+  (is
+   (equalp (docstring-parser::concat-inbetween-text '("foo" "bar"))
+	   (list "foobar")))
 
-(is (equalp (docstring-parser::concat-inbetween-text '("foo" "bar" (:li "lala")))
-	    (list "foobar" '(:li "lala"))))
+  (is (equalp (docstring-parser::concat-inbetween-text '("foo" "bar" (:li "lala")))
+	      (list "foobar" '(:li "lala"))))
 
-(is (equalp (docstring-parser::concat-inbetween-text '("foo" (:li "lala") "bar" ))
-	    '("foo" (:li "lala") "bar" )))
+  (is (equalp (docstring-parser::concat-inbetween-text '("foo" (:li "lala") "bar" ))
+	      '("foo" (:li "lala") "bar" ))))
